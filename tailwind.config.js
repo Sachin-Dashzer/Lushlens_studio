25debfd77736ea770d7119ca358bc91c9cc9fd4c
Sachin Kumar <96949859+Sachin-Dashzer@users.noreply.github.com>
2024-03-15 17:34:29 +0530

    homepage completed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/*.html"],
+  theme: {
+    extend: {
+
+      fontFamily :{
+
+        heading : "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      },
+
+      colors: {
+        primary: "#222223",
+        secondary: "#ffffff",
+        third: "#fbbb48",
+        fourth: "#2a2a2a",
+      },
+    },
+  },
+  plugins: [],
+}
+
